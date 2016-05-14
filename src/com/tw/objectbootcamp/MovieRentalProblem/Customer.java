868f2e6ee4cc19f3b3208d7211b700e5199cbf7c
Shireesha <bongaralashireesha@gmail.com>
2016-05-14 10:44:49 +0530

    Moved methods calculating amount and calculating points

--- src/com/tw/objectbootcamp/MovieRentalProblem/Customer.java
+++ src/com/tw/objectbootcamp/MovieRentalProblem/Customer.java
@@ -25,46 +25,11 @@ public class Customer {
         double totalAmount = 0;
         int totalPoints = 0;
         for (Rental rental : rentalList) {
-            totalAmount += calculateAmount(rental);
-            totalPoints  += calculatePoints(rental);
+            totalAmount += rental.calculateAmount();
+            totalPoints  += rental.calculatePoints();
         }
 
         return "Total Amount : " + totalAmount + "\n" + "Total Points Earned: " + totalPoints + "\n";
     }
 
-    private int calculatePoints(Rental rental) {
-        int points =0;
-        if(rental.get_movie().get_priceCode()== Movie.NEW_RELEASE){
-            points += rental.get_daysRented() - 2;
-        }
-        points++;
-        return points;
-    }
-
-    private double calculateAmount(Rental rental) {
-        double amount =0;
-        switch (rental.get_movie().get_priceCode()) {
-            case Movie.REGULAR:
-                amount += 20;
-                if (rental.get_daysRented() > 2) {
-                    amount += (rental.get_daysRented() - 2) * 15;
-                }
-                break;
-            case Movie.CHILDRENS:
-                amount += 15;
-                if (rental.get_daysRented() > 2) {
-                    amount += (rental.get_daysRented() - 2) * 10;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                amount += 50;
-                if (rental.get_daysRented() > 2) {
-                    amount += (rental.get_daysRented() - 2) * 25;
-
-                }
-                break;
-
-        }
-        return amount;
-    }
 }
--- src/com/tw/objectbootcamp/MovieRentalProblem/Rental.java
+++ src/com/tw/objectbootcamp/MovieRentalProblem/Rental.java
@@ -18,4 +18,40 @@ public class Rental {
     public int get_daysRented() {
         return _daysRented;
     }
+
+    public double calculateAmount() {
+        double amount =0;
+        switch (get_movie().get_priceCode()) {
+            case Movie.REGULAR:
+                amount += 20;
+                if (get_daysRented() > 2) {
+                    amount += (get_daysRented() - 2) * 15;
+                }
+                break;
+            case Movie.CHILDRENS:
+                amount += 15;
+                if (get_daysRented() > 2) {
+                    amount += (get_daysRented() - 2) * 10;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                amount += 50;
+                if (get_daysRented() > 2) {
+                    amount += (get_daysRented() - 2) * 25;
+
+                }
+                break;
+
+        }
+        return amount;
+    }
+
+    public int calculatePoints() {
+        int points =0;
+        if(get_movie().get_priceCode()== Movie.NEW_RELEASE){
+            points += get_daysRented() - 2;
+        }
+        points++;
+        return points;
+    }
 }
